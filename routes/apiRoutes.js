1454f4fc4433ba4ac435d3d20878ae41d68840bc
Stephanie <whatawhat@hotmail.com>
2021-07-09 11:01:44 -0400

    created the get and post routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,34 +1,31 @@
 const fs = require('fs');
+const uniqid = require('uniqid');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
-        res.json(data);
+        fs.readFile("./db/db.json", (error, data) => {
+            if (error) throw error;
+            res.send(data);
+        })
     });
 
     app.post('/api/notes', (req, res) => {
+        const id = uniqid();
         const newNote = req.body;
-        
+        newNote.id = id;
+        //console.log(newNote);
+        //const newNote = req.body.uniqid();
+        fs.readFile("./db/db.json", (error, data) => {
+            if (error) throw error;
+            var dataNote = JSON.parse(data);
+            dataNote.push(newNote);
+            console.log(dataNote);
+        fs.writeFile("./db/db.json", JSON.stringify(dataNote), error => {
+            if (error) throw error;
+            res.send(newNote)
+        })
+        })
 
     })
 }
-
-//Sample Code from Class
-
-// Create New Characters - takes in JSON input
-// app.post('/api/characters', (req, res) => {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-  
-    // const newCharacter = req.body;
-  
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    //This says if there are any spaces, then replace with nothing and then it makes it lowercase so it's not case sensitive
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    //get the object and name and remove spaces and make sure it's lower case and it creates the routeName
-    // newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
-    // console.log(newCharacter);
-  //this pushes it to the array
-   // characters.push(newCharacter);
-    // res.json(newCharacter);
-  //});
-  
+ 
